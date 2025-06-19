8f4d0075f0406e497eaca8ed133c0f370cea88ba
Cal Monica Chen <monicachen808@gmail.com>
2025-06-19 11:17:46 -0400

    updated js file

---
+++ index.js
@@ -0,0 +1,20 @@
+let inputText = document.getElementById('textInput')
+const arrowButton = document.getElementById('arrowButton')
+const xButton = document.getElementById('xButton')
+let choresArray = []
+let choresList = document.getElementById('choresList')
+
+arrowButton.addEventListener("click", function(){
+    choresArray.push(inputText.value)
+    updateList(choresArray.length-1)
+    inputText.value=""
+})
+
+xButton.addEventListener("click", function(){
+    choresArray=[]
+    choresList.innerHTML=""
+})
+
+function updateList(index){
+    choresList.innerHTML+=`<li> ${choresArray[index]} </li>`
+}
